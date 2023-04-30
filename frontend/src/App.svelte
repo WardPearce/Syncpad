5c4116fae48ae452ec271cc07ab8812359ddfec8
WardPearce <wardpearce@protonmail.com>
2023-04-30 20:03:26 +1200

    Cleaned up nav

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { Router, link, Route } from "svelte-navigator";
+  import { Router, Route } from "svelte-navigator";
   import LazyRoute from "./components/LazyRoute.svelte";
   import PageLoading from "./components/PageLoading.svelte";
-  import { advanceModeStore, themeStore } from "./stores";
-  import { get } from "svelte/store";
-  import { getDynamicTheme } from "./lib/theme";
+  import NavItems from "./components/NavItems.svelte";
 
   const CanaryLoader = () => import("./routes/Canary.svelte");
   const LoginLoader = () => import("./routes/Login/Login.svelte");
@@ -15,34 +12,7 @@
   const DashboardVerifySiteLoader = () =>
     import("./routes/Dashboard/VerifySite.svelte");
 
-  let mode = "dark";
   let mobileNavShow = false;
-
-  function onAdvanceModeToggle() {
-    advanceModeStore.set(!get(advanceModeStore));
-  }
-
-  async function toggleMode() {
-    mode = mode === "dark" ? "light" : "dark";
-    await window.ui("mode", mode);
-    localStorage.setItem("mode", mode);
-    themeStore.set(await getDynamicTheme(mode));
-  }
-
-  onMount(async () => {
-    const localStorageMode = localStorage.getItem("mode");
-    if (localStorageMode) {
-      mode = localStorageMode;
-      await window.ui("mode", mode);
-    } else if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      await window.ui("mode", "dark");
-    }
-
-    themeStore.set(await getDynamicTheme());
-  });
 </script>
 
 <Router primary={false}>
@@ -70,92 +40,11 @@
         <h6 class="max">Canary status</h6>
       </nav>
     </header>
-    <a use:link href="/login" class="row round">
-      <i>login</i>
-      <span>Login</span>
-    </a>
-    <a use:link href="/dashboard/add-site" class="row round">
-      <i>add</i>
-      <span>Add a site</span>
-    </a>
-    <a use:link href="/" class="row round">
-      <i>article</i>
-      <span>About</span>
-    </a>
-    <a
-      target="_blank"
-      referrerpolicy="no-referrer"
-      href="https://github.com/WardPearce/canarystatus.com"
-      class="row round"
-    >
-      <i>code</i>
-      <span>Github</span>
-    </a>
-    <div class="row round">
-      <label class="switch icon">
-        <input
-          type="checkbox"
-          checked={mode === "dark"}
-          on:click={toggleMode}
-        />
-        <span>
-          <i>{`${mode}_mode`}</i>
-        </span>
-      </label>
-      <p><span style="text-transform: capitalize;">{mode}</span> mode</p>
-    </div>
-
-    <div class="row round">
-      <label class="switch">
-        <input type="checkbox" on:click={onAdvanceModeToggle} />
-        <span />
-      </label>
-      <p>Advance mode</p>
-    </div>
+    <NavItems isMobile={true} />
   </div>
 
   <nav class="m l right">
-    <a use:link href="/login">
-      <i>login</i>
-      <span>Login</span>
-    </a>
-    <a use:link href="/dashboard/add-site">
-      <i>add</i>
-      <span>Add a site</span>
-    </a>
-    <a use:link href="/">
-      <i>article</i>
-      <span>About</span>
-    </a>
-    <a
-      target="_blank"
-      referrerpolicy="no-referrer"
-      href="https://github.com/WardPearce/canarystatus.com"
-    >
-      <i>code</i>
-      <span>Github</span>
-    </a>
-    <div>
-      <label class="switch icon">
-        <input
-          type="checkbox"
-          checked={mode === "dark"}
-          on:click={toggleMode}
-        />
-        <span>
-          <i>{`${mode}_mode`}</i>
-        </span>
-      </label>
-      <p><span style="text-transform: capitalize;">{mode}</span> mode</p>
-    </div>
-
-    <div>
-      <label class="switch">
-        <input type="checkbox" on:click={onAdvanceModeToggle} />
-        <span />
-      </label>
-      <p>Advance mode</p>
-    </div>
+    <NavItems isMobile={false} />
   </nav>
 
   <main class="responsive">
---
+++ frontend/src/components/NavItems.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { link } from "svelte-navigator";
+  import { advanceModeStore, themeStore } from "../stores";
+  import { get } from "svelte/store";
+  import { getDynamicTheme } from "../lib/theme";
+
+  export let isMobile: boolean = false;
+
+  let mode = "dark";
+
+  function onAdvanceModeToggle() {
+    advanceModeStore.set(!get(advanceModeStore));
+  }
+
+  async function toggleMode() {
+    mode = mode === "dark" ? "light" : "dark";
+    await window.ui("mode", mode);
+    localStorage.setItem("mode", mode);
+    themeStore.set(await getDynamicTheme(mode));
+  }
+
+  onMount(async () => {
+    const localStorageMode = localStorage.getItem("mode");
+    if (localStorageMode) {
+      mode = localStorageMode;
+      await window.ui("mode", mode);
+    } else if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      await window.ui("mode", "dark");
+    }
+
+    themeStore.set(await getDynamicTheme());
+  });
+</script>
+
+<a use:link href="/login" class={isMobile ? "row round" : ""}>
+  <i>login</i>
+  <span>Login</span>
+</a>
+<a use:link href="/dashboard/add-site" class={isMobile ? "row round" : ""}>
+  <i>add</i>
+  <span>Add a site</span>
+</a>
+<a use:link href="/" class={isMobile ? "row round" : ""}>
+  <i>article</i>
+  <span>About</span>
+</a>
+<a
+  target="_blank"
+  referrerpolicy="no-referrer"
+  href="https://github.com/WardPearce/canarystatus.com"
+  class={isMobile ? "row round" : ""}
+>
+  <i>code</i>
+  <span>Github</span>
+</a>
+<div class={isMobile ? "row round" : ""}>
+  <label class="switch icon">
+    <input type="checkbox" checked={mode === "dark"} on:click={toggleMode} />
+    <span>
+      <i>{`${mode}_mode`}</i>
+    </span>
+  </label>
+  <p><span style="text-transform: capitalize;">{mode}</span> mode</p>
+</div>
+
+<div class={isMobile ? "row round" : ""}>
+  <label class="switch">
+    <input type="checkbox" on:click={onAdvanceModeToggle} />
+    <span />
+  </label>
+  <p>Advance mode</p>
+</div>
