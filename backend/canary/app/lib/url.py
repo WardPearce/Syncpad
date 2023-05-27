a4d7cc5f85b10c3463eb2b852c93de0df208e63a
WardPearce <wardpearce@protonmail.com>
2023-05-27 12:48:32 +1200

    minor changes to untrusted_http_request

--- backend/canary/app/lib/url.py
+++ backend/canary/app/lib/url.py
@@ -16,14 +16,17 @@ if TYPE_CHECKING:
 async def untrusted_http_request(
     state: "State", url: str, method: str, **kwargs
 ) -> ClientResponse:
-    # If no http proxy provided, do our best
-    # to ensure not local, still open to DNS rebinding possibly.
-    if not SETTINGS.untrusted_request_proxy:
-        try:
-            await is_local(url)
-        except LocalDomainInvalid:
-            raise
+    try:
+        await is_local(
+            url,
+            # If no proxy, then try our best to
+            # ensure the request doesn't resolve to a local IP.
+            attempt_dns_resolve=not SETTINGS.untrusted_request_proxy,
+        )
+    except LocalDomainInvalid:
+        raise
 
+    if not SETTINGS.untrusted_request_proxy:
         # Don't allow redirects if no proxy
         kwargs["allow_redirects"] = False
     else:
@@ -51,7 +54,7 @@ async def get_localhost_aliases(resolver: aiodns.DNSResolver) -> List[str]:
     return aliases
 
 
-async def is_local(url: str) -> None:
+async def is_local(url: str, attempt_dns_resolve: bool = True) -> None:
     """Used to validate webhooks if it's a local ip or not.
 
     Raises:
@@ -60,7 +63,7 @@ async def is_local(url: str) -> None:
 
     url_ = yarl.URL(url)
 
-    if url_.scheme != "https":
+    if url_.scheme not in ("https", "http"):
         raise LocalDomainInvalid()
 
     domain = url_.host
@@ -68,29 +71,30 @@ async def is_local(url: str) -> None:
     if not domain:
         raise LocalDomainInvalid()
 
-    resolver = aiodns.DNSResolver()
+    if attempt_dns_resolve:
+        resolver = aiodns.DNSResolver()
 
-    localhost_aliases = await get_localhost_aliases(resolver)
+        localhost_aliases = await get_localhost_aliases(resolver)
 
-    if domain in localhost_aliases:
-        raise LocalDomainInvalid()
-
-    try:
-        given_ip = ip_address(domain)
-        if given_ip.is_private or given_ip.is_loopback:
+        if domain in localhost_aliases:
             raise LocalDomainInvalid()
-    except ValueError:
-        pass
 
-    try:
-        ipv4_address = await resolver.query(domain, "A")
-        for ipv4 in ipv4_address:
-            if ipv4 in localhost_aliases or __is_local_ip(ipv4):
+        try:
+            given_ip = ip_address(domain)
+            if given_ip.is_private or given_ip.is_loopback:
                 raise LocalDomainInvalid()
+        except ValueError:
+            pass
 
-        ipv6_address = await resolver.query(domain, "AAAA")
-        for ipv6 in ipv6_address:
-            if ipv6 in localhost_aliases or __is_local_ip(ipv6):
-                raise LocalDomainInvalid()
-    except (aiodns.error.DNSError, ValueError):
-        raise LocalDomainInvalid()  # Look up fails, assume is private.
+        try:
+            ipv4_address = await resolver.query(domain, "A")
+            for ipv4 in ipv4_address:
+                if ipv4 in localhost_aliases or __is_local_ip(ipv4):
+                    raise LocalDomainInvalid()
+
+            ipv6_address = await resolver.query(domain, "AAAA")
+            for ipv6 in ipv6_address:
+                if ipv6 in localhost_aliases or __is_local_ip(ipv6):
+                    raise LocalDomainInvalid()
+        except (aiodns.error.DNSError, ValueError):
+            raise LocalDomainInvalid()  # Look up fails, assume is private.
