b1a1c52ec5adac559c280125d43b4d102426f0d7
WardPearce <wardpearce@protonmail.com>
2023-06-17 11:26:23 +1200

    Added PWA support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 import viteCompression from "vite-plugin-compression";
+import { VitePWA } from 'vite-plugin-pwa';
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
@@ -10,6 +11,23 @@ export default defineConfig({
     svelte(),
     wasm(),
     topLevelAwait(),
-    viteCompression({ algorithm: "brotliCompress" })
+    viteCompression({ algorithm: "brotliCompress" }),
+    VitePWA({
+      manifest: {
+        name: "Purplix.io",
+        short_name: "Purplix",
+        description: "Not completed",
+        icons: [
+          {
+            src: "/android-icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            density: "4.0"
+          }
+        ],
+        background_color: "#161418",
+        theme_color: "#8749f4"
+      }
+    })
   ]
 });
