3f9aaf6a8afd3cb686697ba4222e516a0a086987
WardPearce <wardpearce@protonmail.com>
2023-07-04 00:30:45 +1200

    Redesign of survey builder

--- frontend/src/components/Survey/modes.ts
+++ frontend/src/components/Survey/modes.ts
@@ -1,5 +1,5 @@
 export enum SurveyComponentsTypes {
-  preview,
+  create,
   input,
   result
 }
--- frontend/src/routes/Dashboard/Survey/Create.svelte
+++ frontend/src/routes/Dashboard/Survey/Create.svelte
@@ -1,62 +1,57 @@
-<script lang="ts">
-    import type { ComponentType } from "svelte";
-    import TextArea from "../../../components/Survey/TextArea.svelte";
-    import TextInput from "../../../components/Survey/TextInput.svelte";
-    import { SurveyComponentsTypes } from "../../../components/Survey/modes";
-
-    let surveyStructure: ComponentType[] = [];
-
-    let surveyTitle = "Untitled Survey";
-    let editTitle = false;
-
-    const surveyComponents = [
-        { name: "Text area", component: TextArea },
-        { name: "Text input", component: TextInput },
-    ];
-
-    function addComponent(component: ComponentType) {
-        surveyStructure = [...surveyStructure, component];
-    }
-</script>
-
-{#if !editTitle}
-    <nav>
-        <h3>{surveyTitle}</h3>
-        <button class="square round" on:click={() => (editTitle = true)}>
-            <i>create</i>
-        </button>
-    </nav>
-{:else}
-    <form on:submit|preventDefault={() => (editTitle = false)}>
-        <nav style="margin-top: 1em;">
-            <div class="field border">
-                <input type="text" bind:value={surveyTitle} />
+<article>
+    <div class="grid">
+        <div class="s12 m6 l7">
+            <div class="field border large fill">
+                <input type="text" value="Untitled Question" />
             </div>
-            <button class="square round">
-                <i>check</i>
-            </button>
-        </nav>
-    </form>
-{/if}
-
-{#each surveyStructure as surveyComp}
-    <svelte:component this={surveyComp} mode={SurveyComponentsTypes.preview} />
-{/each}
-
-<article class="border no-padding middle-align center-align">
-    <div class="padding">
-        <button type="button">
-            <i>add</i>
-            <span>Add question</span>
-            <menu>
-                {#each surveyComponents as survey}
-                    <a
-                        href={`#${survey.name}`}
-                        on:click={() => addComponent(survey.component)}
-                        >{survey.name}</a
-                    >
-                {/each}
-            </menu>
-        </button>
+        </div>
+        <div class="s12 m6 l3">
+            <div class="field suffix large border">
+                <select>
+                    <option>Short answer</option>
+                    <option>Paragraph</option>
+                    <option>Multiple choice</option>
+                    <option>Checkbox</option>
+                </select>
+                <i>arrow_drop_down</i>
+            </div>
+        </div>
     </div>
+    <article class="surface-variant">
+        <nav class="right-align">
+            <button class="border">
+                <i>add</i>
+                <span>Add Description</span>
+            </button>
+
+            <button class="border">
+                <i>done_all</i>
+                <span>Regex</span>
+            </button>
+
+            <button class="square border tertiary-border tertiary-text round">
+                <i>delete</i>
+                <div class="tooltip">Delete question</div>
+            </button>
+
+            <button class="square border round">
+                <i>content_copy</i>
+                <div class="tooltip">Duplicate question</div>
+            </button>
+
+            <label class="switch">
+                <input type="checkbox" />
+                <span style="padding-left: .3em;">Required</span>
+            </label>
+        </nav>
+    </article>
 </article>
+
+<style>
+    @media only screen and (max-width: 800px) {
+        nav {
+            align-items: start;
+            flex-direction: column;
+        }
+    }
+</style>
