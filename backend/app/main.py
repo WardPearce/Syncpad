a39fb9331de20d862f78eda08ce083d660e5c46c
WardPearce <wardpearce@pm.me>
2023-10-08 11:48:17 +1300

    Fixed linting

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,6 @@ from litestar.middleware.rate_limit import RateLimitConfig
 from litestar.openapi import OpenAPIConfig
 from litestar.openapi.spec import License, Server
 from litestar.stores.redis import RedisStore
-from models.customs import TYPE_ENCODERS
 from motor import motor_asyncio
 from redis.asyncio import Redis
 
@@ -20,6 +19,7 @@ from app.controllers import routes
 from app.env import SETTINGS
 from app.lib.crontabs import CronTabs
 from app.lib.jwt import jwt_cookie_auth
+from app.models.customs import TYPE_ENCODERS
 from app.tasks import tasks
 
 if TYPE_CHECKING:
