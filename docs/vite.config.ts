c7e2bd6d05aed6a8076d958fdfe6594ffa491b7b
WardPearce <wardpearce@pm.me>
2023-10-09 16:16:22 +1300

    Added redoc schema

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,34 +1,4 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import react from '@vitejs/plugin-react';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      plugins: [
-        nodeResolve(),
-        commonjs(),
-      ],
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
 });
