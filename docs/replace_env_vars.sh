081251beb84ab15e5d7e1b50d1fcb2daa2106122
WardPearce <wardpearce@pm.me>
2024-07-26 19:27:55 +1200

    Renamed old frontend

--- docs/replace_env_vars.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ROOT_DIR=./
-
-# Replace env vars in JavaScript and HTML files served by NGINX
-find "$ROOT_DIR" -type f \( -name "*.js" -o -name "*.html" \) -exec sed -i 's|VITE_API_SCHEMA_URL_PLACEHOLDER|'"$VITE_API_SCHEMA_URL"'|g' {} +
